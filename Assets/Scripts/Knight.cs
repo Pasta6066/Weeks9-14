7870c87e16fdad2c4c20445076c8661e2a251ac6
Jackson Driver <driverja@shernet.sheridancollege.ca>
2025-04-03 15:36:36 -0400

    Night walks around on ground and makes it shake and plays a sound when foot hits ground. Camera follows the night

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -9,7 +10,9 @@ public class Knight : MonoBehaviour
     Animator animator;
     public float speed = 2;
     public bool canRun = true;
-
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public CinemachineImpulseSource impulseSource;
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
@@ -21,12 +24,19 @@ public class Knight : MonoBehaviour
     {
 
         float direction = Input.GetAxisRaw("Horizontal");
+        float direction2 = Input.GetAxisRaw("Vertical");
+        float directionCheck = direction;
+
+        directionCheck += direction + direction2;
+        Debug.Log(direction2);
 
         sr.flipX = (direction < 0);
-        animator.SetFloat("movement", Mathf.Abs(direction));
+        animator.SetFloat("movement", Mathf.Abs(directionCheck));
+        animator.SetFloat("movement2", Mathf.Abs(direction2));
 
-       
-        if(canRun == true)
+
+
+        if (canRun == true)
         {
             if (Input.GetMouseButtonDown(0))
             {
@@ -35,6 +45,7 @@ public class Knight : MonoBehaviour
             }
 
             transform.position += transform.right * direction * speed * Time.deltaTime;
+            transform.position += transform.up * direction2 * speed * Time.deltaTime;
         }
     }
 
@@ -42,5 +53,14 @@ public class Knight : MonoBehaviour
     {
         Debug.Log("The Attack Animation Has Finished!");
         canRun = true;
+    }
+
+    public void FootHasHitGround()
+    {
+        if (audioSource.isPlaying == false)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+        impulseSource.GenerateImpulse();
     }
 }
