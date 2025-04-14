c5d314b7ffbe02f58252b2978a3cf5ecc28da8b8
Pasta6066 <jackdrive239@gmail.com>
2025-04-13 23:28:25 -0400

    made blocks spawn in a grid

--- Assets/Scripts/BlockCollision.cs
+++ Assets/Scripts/BlockCollision.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class BlockCollision : MonoBehaviour
 {
+    public GameObject block;
+    public int rows = 6;
+    public int columns  = 7;
+    public float spacing = 1.5f;
+    public Vector2 startPoint = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gridSpawner();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void gridSpawner()
+    {
+        for (int i = 0;  i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Vector2 spawnPosition = new Vector2(startPoint.x + (j * spacing), startPoint.y + (i * spacing));
+                Instantiate(block, spawnPosition, Quaternion.identity);
+            }
+        }
     }
 }
